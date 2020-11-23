c7861593db06d76f5fb8c87ca86fad652a906d72
meltvschoor <73581384+meltvschoor@users.noreply.github.com>
2020-11-23 12:49:55 +0200

    Changed file path

--- resources/read_conf.py
+++ resources/read_conf.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 def retrieve_variable(variable):
 
     config_object = ConfigParser()
-    config_object.read(".clinic_config/clinic.conf")
+    config_object.read("resources/.clinic_config/clinic.conf")
 
     userinfo = config_object["user_info"]
     
